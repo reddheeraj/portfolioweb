a02f418cc83212d6622ece9896ff10040ba14973
Dheeraj Reddy <meetdheerajreddy@gmail.com>
2023-09-03 15:53:34 +0530

    check

--- src/pages/ProjDisp.js
+++ src/pages/ProjDisp.js
@@ -9,34 +9,35 @@ function ProjDisp() {
     const { id } = useParams()
     const project = projectList[id]
     return (
-    <div className='project'>
-        <p> </p>
-        <h1>{project.name}</h1>
-        <img src={project.image} alt={project.name}/>
-        <div className='descbox'>
-            <p>
-                <b>Description: </b> {project.description}
-            </p>
-            <p>
-                <b>Tech used: </b> {project.tech}
-            </p>
+        <div className='project'>
+            <p> </p>
+            <h1>{project.name}</h1>
+            <img src={project.image} alt={project.name} />
+            <div className='descbox'>
+                <p>
+                    <b>Description: </b> {project.description}
+                </p>
+                <p>
+                    <b>Tech used: </b> {project.tech}
+                </p>
+            </div>
+            <div className="butlinks">
+                <a href={project.github} target="_blank" rel="noreferrer">
+                    <button className="github">
+                        <GithubIcon />
+                        View on Github
+                    </button>
+                </a>
+                <a href={project.live} target="_blank" rel="noreferrer">
+                    <button className="live">
+                        <CodeIcon />
+                        View Live
+                    </button>
+                </a>
+            </div>
         </div>
-        <div className="butlinks">
-            <a href={project.github} target="_blank" rel="noreferrer">
-                <button className="github">
-                    <GithubIcon />
-                    View on Github
-                </button>
-            </a>
-            <a href={project.live} target="_blank" rel="noreferrer">
-                <button className="live">
-                    <CodeIcon />
-                    View Live
-                </button>
-            </a>
-        </div>
-    </div>
-  )
+    )
+
 }
 
 export default ProjDisp
